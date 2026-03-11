f9b53cecb46a4c0a01daffdeb2a526dd70ff1e8f
Maksymyr <maksymyr@gmail.com>
2026-03-11 20:31:26 +0100

    ITT: expand CRUD commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,3 +1,7 @@
-from .contacts import add_contact, change_contact, show_phone, show_all, delete_contact, delete_phone, find_phone_owner
+from .contacts import (
+    add_contact, change_contact, show_phone, show_all,
+    delete_contact, delete_phone, find_phone_owner,
+    add_email, add_address, search_contact, edit_contact,
+)
 from .birthdays import add_birthday, show_birthday, birthdays
 from .system import hello
