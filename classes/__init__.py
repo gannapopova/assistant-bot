6e87cccfcb156ae6731ec8069da107f176af5a18
Maksymyr <maksymyr@gmail.com>
2026-03-12 00:32:18 +0100

    ITT: create notes

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,3 +1,5 @@
 from .fields import Field, Name, Phone, Birthday, Email, Address
 from .address_book import Record, AddressBook
 from .repositories import ContactRepository, NoteRepository
+from .notes import Note
+from .note_book import NoteBook
